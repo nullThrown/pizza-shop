250d6566c825000475378be7dbb663397e3ee23a
Weston Peters <westonrpr@yahoo.com>
2021-05-04 14:30:31 -0500

    Add all food item objects

---
+++ js/models/foodItems.js
@@ -0,0 +1,208 @@
+let foodItems = [
+  {
+    category: 'pizza',
+    name: 'Cheese Pizza',
+    smallPrice: 9.99,
+    mediumPrice: 11.99,
+    largePrice: 13.99,
+    xlargePrice: 16.99,
+    uuid: 'piz100'    
+  },
+  {
+    category: 'pizza',
+    name: 'Pepperoni Pizza',
+    smallPrice: 9.99,
+    mediumPrice: 11.99,
+    largePrice: 13.99,
+    xlargePrice: 16.99,
+    uuid: 'piz200'
+  },
+  {
+    category: 'pizza',
+    name: 'Works Pizza',
+    smallPrice: 10.99,
+    mediumPrice: 13.99,
+    largePrice: 16.99,
+    xlargePrice: 19.99,
+    uuid: 'piz300'
+  },
+  {
+    category: 'pizza',
+    name: 'Sausage Pizza',
+    smallPrice: 9.99,
+    mediumPrice: 11.99,
+    largePrice: 13.99,
+    xlargePrice: 16.99,
+    uuid: 'piz400'
+  }, 
+  {
+    category: 'pizza',
+    name: 'Meats Pizza',
+    smallPrice: 10.99,
+    mediumPrice: 13.99,
+    largePrice: 16.99,
+    xlargePrice: 19.99,
+    uuid: 'piz500'
+  },
+  {
+    category: 'pizza',
+    name: 'Vegetarian Pizza',
+    smallPrice: 10.99,
+    mediumPrice: 13.99,
+    largePrice: 16.99,
+    xlargePrice: 19.99,
+    uuid: 'piz600'
+  },
+  {
+    category: 'pizza',
+    name: 'Hawaiian Pizza',
+    smallPrice: 10.99,
+    mediumPrice: 13.99,
+    largePrice: 16.99,
+    xlargePrice: 19.99,
+    uuid: 'piz700'
+  },
+  {
+    category: 'pizza',
+    name: 'BBQ Chicken Pizza',
+    smallPrice: 10.99,
+    mediumPrice: 13.99,
+    largePrice: 16.99,
+    xlargePrice: 19.99,
+    uuid: 'piz800'
+  },
+  {
+    category: 'pizza',
+    name: 'Six Cheese Pizza',
+    smallPrice: 10.99,
+    mediumPrice: 13.99,
+    largePrice: 16.99,
+    xlargePrice: 19.99,
+    uuid: 'piz900'
+  },
+  {
+    category: 'side',
+    name: 'Jalepeno Poppers',
+    smallPrice: 3.99,
+    largePrice: 5.99,
+    uuid: 'sde100'
+  }, 
+  {
+    category: 'side',
+    name: 'Garlic Bread',
+    price: 3.99,
+    uuid: 'sde200'
+  },
+  {
+    category: 'side',
+    name: 'Parmesan Breadsticks',
+    smallPrice: 2.99,
+    largePrice: 4.99,
+    uuid: 'sde300'
+  }, 
+  {
+    category: 'side',
+    name: 'Chicken Wings',
+    smallPrice: 3.99,
+    largePrice: 5.99,
+    uuid: 'sde400'
+  }, 
+  {
+    category: 'side',
+    name: 'Fries',
+    smallPrice: 1.39,
+    mediumPrice: 1.79,
+    largePrice: 1.99,
+    uuid: 'sde500'
+  }, 
+  {
+    category: 'side',
+    name: 'Garlic Fries',
+    smallPrice: 1.75,
+    mediumPrice: 2.39,
+    largePrice: 2.99,
+    uuid: 'sde600'
+  }, 
+  {
+    category: 'side',
+    name: 'Chicken Nuggets',
+    smallPrice: 1.99,
+    mediumPrice: 2.75,
+    largePrice: 3.99,
+    uuid: 'sde700'
+  }, 
+  {
+    category: 'side',
+    name: 'Caesar Salad',
+    smallPrice: 2.99,
+    largePrice: 4.99,
+    uuid: 'sde800'
+  }, 
+  {
+    category: 'side',
+    name: 'Garden Salad',
+    smallPrice: 2.99,
+    largePrice: 4.99,
+    uuid: 'sde900'
+  }, 
+  {
+    category: 'dessert',
+    name: 'Cinnamon Sticks',
+    price: 5.99,
+    uuid: 'des100'
+  },
+  {
+    category: 'dessert',
+    name: 'Chocolate Chip Cookie',
+    price: 1.99,
+    uuid: 'des200'
+  },
+  {
+    category: 'dessert',
+    name: 'Chocolate Brownie',
+    price: 4.99,
+    uuid: 'des300'
+  },
+  {
+    category: 'drink',
+    name: 'Coke',
+    smallPrice: 1.75,
+    largePrice: 2.99,
+    uuid: 'drnk100'
+  },
+  {
+    category: 'drink',
+    name: 'Diet Coke',
+    smallPrice: 1.75,
+    largePrice: 2.99,
+    uuid: 'drnk200'
+  },
+  {
+    category: 'drink',
+    name: 'Root Beer',
+    smallPrice: 1.75,
+    largePrice: 2.99,
+    uuid: 'drnk300'
+  },
+  {
+    category: 'drink',
+    name: 'Sprite',
+    smallPrice: 1.75,
+    largePrice: 2.99,
+    uuid: 'drnk400'
+  },
+  {
+    category: 'drink',
+    name: 'Dr Pepper',
+    smallPrice: 1.75,
+    largePrice: 2.99,
+    uuid: 'drnk500'
+  },
+  {
+    category: 'drink',
+    name: 'Grape Crush',
+    smallPrice: 1.75,
+    largePrice: 2.99,
+    uuid: 'drnk600'
+  }
+];
