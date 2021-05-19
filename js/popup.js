d14342b31b06c558f3ecdcad6fc7123756c5649e
Weston Peters <westonrpr@yahoo.com>
2021-05-19 13:36:03 -0500

    Remove code for deals and cart specific behavior

---
+++ js/popup.js
@@ -0,0 +1,34 @@
+
+// change class names for menu pages -- they still have popup--failure attached to them
+const popup = document.querySelector('.popup');
+const popupIcon = document.querySelector('.popup__icon');
+const popupText = document.querySelector('.popup__text');
+
+
+function activateSuccessPopup() {
+  popup.classList.add('popup--success');
+  popupIcon.classList.add('popup__icon--success');
+  popup.style.display = 'flex';
+  setTimeout(() => {
+    popup.style.display = 'none';
+    popup.classList.remove('popup--success');
+  popupIcon.classList.remove('popup__icon--success');
+  }, 3000);
+};
+
+function activateFailurePopup() {
+  popup.classList.add('popup--failure');
+  popupIcon.classList.add('popup__icon--failure');
+  popup.style.display = 'flex';
+  setTimeout(() => {
+    popup.style.display = 'none';
+    popup.classList.remove('popup--failure');
+  popupIcon.classList.remove('popup__icon--failure');
+  }, 3000);
+};
+
+
+
+
+
+
