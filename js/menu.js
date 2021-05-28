fc2360ab37e7b34d9528f42ffe376a135ab99eec
Weston Peters <westonrpr@yahoo.com>
2021-05-28 16:57:09 -0500

    see description
    *move determine price fn() into menu.js file *change determinePrice fn() to return object with props for calculated price and original price *add uuid and original price props to cartItem

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,42 @@ const itemCountSelectors = document.querySelectorAll('.food-item-count');
 const addToCartBtns = document.querySelectorAll('.btn--add-to-cart');
 
 
+// determines price of specific food item based on size, type, count etc.
+function determinePrice(size, count, uuid) {
+  let foodItem = foodItems.find(foodItem => foodItem.uuid == uuid); 
+   let price = 0;
+ 
+   if(!size) {
+     price =  foodItem.price;
+   }
+   else {
+     switch(size) {
+       case 'small':
+         price = foodItem.smallPrice;
+         break;
+       case 'medium':
+         price = foodItem.mediumPrice;
+         break;
+       case 'large':
+         price = foodItem.largePrice;
+         break;
+       case 'x-large':
+         price = foodItem.xlargePrice;
+         break;
+     };
+   };
+     return {
+       totalPrice: Number((price * Number(count)).toFixed(2)), 
+       originalPrice: price
+      }
+   }; 
+
 itemSizeSelecters.forEach(sizeSelector => {
   sizeSelector.addEventListener('change', () => {
    let priceEl = sizeSelector.parentNode.querySelector('.item-price__amount');
    let countEl = sizeSelector.parentNode.querySelector('.food-item-count').value;
    let uuid = sizeSelector.parentNode.dataset.uuid;
-   priceEl.textContent = determinePrice(sizeSelector.value, countEl, uuid);
+   priceEl.textContent = determinePrice(sizeSelector.value, countEl, uuid).totalPrice;
   });
 });
 
@@ -20,38 +50,42 @@ itemCountSelectors.forEach(countSelector => {
       sizeSelectEl = sizeSelectEl.value;
     }
     let uuid = countSelector.parentNode.dataset.uuid;
-    priceEl.textContent = determinePrice(sizeSelectEl, countSelector.value, uuid);
+    priceEl.textContent = determinePrice(sizeSelectEl, countSelector.value, uuid).totalPrice;
   });
 }); 
-
 
 addToCartBtns.forEach(btn => {
   btn.addEventListener('click', (e) => {
     let foodItemBox = e.target.closest('.food-item-box');
     let sizeSelectEl = foodItemBox.querySelector('.item-size-select') || undefined;
-    if(sizeSelectEl !== undefined){
-      sizeSelectEl = sizeSelectEl.value;
-    }
+    if(sizeSelectEl !== undefined) sizeSelectEl = sizeSelectEl.value;
+  
     let crustSelectEl = foodItemBox.querySelector('.pizza-crust-select') || undefined;
-      if(crustSelectEl !== undefined) {
-        crustSelectEl = crustSelectEl.value;
-      }
+      if(crustSelectEl !== undefined) crustSelectEl = crustSelectEl.value;
 
     let sauceSelectEl = foodItemBox.querySelector('.item-sauce-select') || undefined;
-      if(sauceSelectEl !== undefined) {
-        sauceSelectEl = sauceSelectEl.value;
-      }
+      if(sauceSelectEl !== undefined) sauceSelectEl = sauceSelectEl.value;
     
     let countEl = foodItemBox.querySelector('.food-item-count');
-    let currentPrice = determinePrice(sizeSelectEl, countEl.value, foodItemBox.dataset.uuid);
+    let {totalPrice, originalPrice } = determinePrice(sizeSelectEl, countEl.value, foodItemBox.dataset.uuid);
     let foodItem = foodItems.find(foodItem => foodItem.uuid === foodItemBox.dataset.uuid);
-   
-    let cartItem = new CartItem(foodItem.name, sizeSelectEl, crustSelectEl, sauceSelectEl, countEl.value, currentPrice);
+    let uuid = create_UUID();
+      
+    let cartItem = new CartItem
+    ( 
+      uuid,
+      foodItem.name, 
+      sizeSelectEl, 
+      crustSelectEl, 
+      sauceSelectEl, 
+      countEl.value,
+      originalPrice, 
+      totalPrice
+    );
     addItemToLocalStorage(cartItem);
 
     activatePopupOnCart();
-    renderNewCartItem();
-    renderCartCount();
+    renderCart();
   });
 });
 
